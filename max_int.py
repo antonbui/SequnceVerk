a7cf589ba7b9967cb7699f53a93ccb48c0ee9dd3
Anton Jónsson <anton17@ru.is>
2018-09-04 14:54:50 +0000

    Sett inn gefinn kóða

--- max_int.py
+++ max_int.py
@@ -1,3 +1,12 @@
 #Forrit þar sem user seturinn inn int þangað til að hann setur negatíva tölu og þá stoppar forritið
 
 
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = num_int
+while num_int > 0:
+    num_int = int(input("Input a number: "))
+    if max_int < num_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
+
