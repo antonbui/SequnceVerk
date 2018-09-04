ee9c55d9998c488c994b8a8fc7eef15458bbb95d
Anton Jónsson <anton17@ru.is>
2018-09-04 15:43:42 +0000

    Comments added

--- sequence.py
+++ sequence.py
@@ -1,3 +1,19 @@
 #Forrit sem reiknar út sequence-ið  1, 2, 3, 6, 11, 20, 37, ___, ___, ___, ...
 
-n = int(input("Enter the length of the sequence: ")) # Do not change this line
+n = int(input("Enter the length of the sequence: ")) # Do not change this linegit 
+
+num1 = 1
+num2 = 2
+num3 = 3
+
+for i in range(1, n+1):
+    if i < 4:
+        print(i)
+    else:
+        nextnum = num1 + num2 + num3
+        num1 = num2
+        num2 = num3
+        num3 = nextnum
+        print(nextnum)
+    
+
